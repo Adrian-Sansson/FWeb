e3057f47b02aa6132835d0dacdff3a1d650872ca
bruna <bruna.gpires@hotmai.com>
2024-08-09 21:22:20 -0300

    Ultimo Commit Aqui

--- aulas/aula20/exercicio2/src/App.js
+++ aulas/aula20/exercicio2/src/App.js
@@ -3,13 +3,27 @@ import './components1.css';
 import Login from './components1.js';
 import Username from './components2.js';
 import Password from './components3.js';
+import End from './components4.js';
 
 function App() {
   return (
     <div className="container">
-      <Login name/>
-      <Username name1/>
-      <Password name2/>
+      <div className="containerTwo">
+        <div className="components1">
+          <Login name/>
+        </div>
+        <div className="containerTre">
+          <div className="components2">
+            <Username name1=<input id="botao1" bottom></input> />
+          </div>
+          <div className="components3">
+            <Password name2=<input id="botao2" bottom></input> />
+          </div>
+        </div>
+        <div className="components4">
+          <End />
+        </div>
+      </div>
     </div>
   );
 }
